5b508da683e62c8a121d0490981731bb6fb4bc4c
Student User <student@workstation.lab.example.com>
2020-12-05 15:27:55 -0500

    chnage index page JIRA001

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A chage is mady by developer A team!\n"
-Print " Please contact to abc@abc.com for any query."
+print "A chage is mady by developer A team!\n";
+Print " Please contact to abc@abc.com for any query.";
 ?>
